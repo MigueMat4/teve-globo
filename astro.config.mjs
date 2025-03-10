57dd3b8297b645e7f882455bb17ab768db7e8629
MigueMat4 <miguemat4@gmail.com>
2025-03-09 23:01:57 -0600

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  site: "https://teveglobo.live/",
   integrations: [tailwind(), icon()],
-  adapter: node({
-    mode: "standalone"
-  }),
 });
